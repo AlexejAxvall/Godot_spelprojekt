f25ea8990028565b04d2c36dcdf8d0e064056a71
Johnny Stahel <h22josa@axevalla.fhsk.se>
2024-03-08 09:05:21 +0100

    attack

--- Player/Player1.gd
+++ Player/Player1.gd
@@ -9,6 +9,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animation = get_node("AnimationPlayer")
 
+var health = 10
+
 func _ready():
 	animation.play("Idle")
 
@@ -20,27 +22,41 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	
+	var direction_up_down = Input.get_axis("ui_down","ui_up")
+	
+	#if direction_up_down == -1:
+		#animation.play("down")
+	#elif direction_up_down == 1:
+		#animation.play("up")
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction_left_right = Input.get_axis("ui_left", "ui_right")
 	
-	if direction == -1:
+	if direction_left_right == -1:
 		get_node("AnimatedSprite2D").flip_h = true
-	elif direction == 1:
+	elif direction_left_right == 1:
 		get_node("AnimatedSprite2D").flip_h = false
 	
-	if direction:
-		velocity.x = direction * SPEED
+	if direction_left_right:
+		velocity.x = direction_left_right * SPEED
 		if velocity.y == 0 and is_on_floor():
 			animation.play("Run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0 and is_on_floor():
+		if velocity.y == 0 and velocity.x == 0 and is_on_floor() and Input.is_action_just_pressed("Player1_attack") != true:
 			animation.play("Idle")
 	
 	if not is_on_floor():
 		animation.play("Jump")
 	
-	
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("Player1_attack"):
+		animation.play("Attack")
+	
+		
+
+func take_damage(damage):
+	health -= damage
