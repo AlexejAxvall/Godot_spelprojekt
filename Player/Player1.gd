d337062d73431bffa5809436ba9e9b0042f3f8e3
Johnny Stahel <h22josa@axevalla.fhsk.se>
2024-03-08 12:03:37 +0100

    attack

--- Player/Player1.gd
+++ Player/Player1.gd
@@ -9,10 +9,15 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animation = get_node("AnimationPlayer")
 
+#var player1_scene = preload("res://Player/Player1.tscn")
+var able = true
+
+var stock = 3
 var health = 10
 
 func _ready():
 	animation.play("Idle")
+	
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,32 +25,32 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and animation.current_animation != "Attack":
 		velocity.y = JUMP_VELOCITY
 	
 	var direction_up_down = Input.get_axis("ui_down","ui_up")
 	
-	#if direction_up_down == -1:
-		#animation.play("down")
-	#elif direction_up_down == 1:
-		#animation.play("up")
+	if direction_up_down == -1:
+		animation.play("down")
+	elif direction_up_down == 1:
+		animation.play("up")
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions
 	var direction_left_right = Input.get_axis("ui_left", "ui_right")
 	
-	if direction_left_right == -1:
+	if direction_left_right == -1 and animation.current_animation != "Attack":
 		get_node("AnimatedSprite2D").flip_h = true
-	elif direction_left_right == 1:
+	elif direction_left_right == 1 and animation.current_animation != "Attack":
 		get_node("AnimatedSprite2D").flip_h = false
 	
-	if direction_left_right:
+	if direction_left_right and animation.current_animation != "Attack":
 		velocity.x = direction_left_right * SPEED
 		if velocity.y == 0 and is_on_floor():
 			animation.play("Run")
-	else:
+	elif animation.current_animation != "Attack":
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0 and velocity.x == 0 and is_on_floor() and Input.is_action_just_pressed("Player1_attack") != true:
+		if velocity.y == 0 and velocity.x == 0 and is_on_floor() and animation.current_animation != "Attack":
 			animation.play("Idle")
 	
 	if not is_on_floor():
@@ -53,10 +58,19 @@ func _physics_process(delta):
 	
 	move_and_slide()
 	
-	if Input.is_action_just_pressed("Player1_attack"):
+	if Input.is_action_just_pressed("Player1_attack") and is_on_floor():
 		animation.play("Attack")
+		velocity.x = 0
 	
-		
-
+	take_damage(1)
+	
 func take_damage(damage):
 	health -= damage
+	if health <= 0 and able:
+		self.process_mode = 4
+		self.hide()
+		stock -= 1
+		var cooldown_spawner = Timer.new()
+		cooldown_spawner.autostart = true
+		cooldown_spawner.wait_time = 1
+	#	cooldown_spawner.connect("timeout", self, "spawn_player")
